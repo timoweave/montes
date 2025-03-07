f26f3c3995432b21c389a30a03b5e5ed9048dc38
Timothy Shiu <timoweave@gmail.com>
2025-03-07 14:13:46 -0800

    init

---
+++ app.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+x = np.linspace(0, 2 * np.pi, 100)
+
+y = np.sin(x)
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+plt.title('Sine Curve')
+plt.show()
+
+
+plt.plot(np.random.randn(1000).cumsum())
+plt.show()
